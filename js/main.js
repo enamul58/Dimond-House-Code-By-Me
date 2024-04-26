2426df73a67b72d4b649ff0a1166bfc60dc675ff
“enamul58” <enamullimon@gmail.com>
2024-04-26 11:59:35 +0600

    creat responsive navbar &

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let navBar = document.querySelector(".navbar");
+let navBar = document.querySelector(".navigation_wrapper");
 
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
@@ -7,3 +7,12 @@ window.onscroll = function () {
     navBar.classList.remove("on_scroll");
   }
 };
+
+//For Close Focus Navbar
+let navLink = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse");
+navLink.forEach(function (singleLink) {
+  singleLink.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
